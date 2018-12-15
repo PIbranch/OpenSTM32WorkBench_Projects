537777e20040f60cadb2b87a0b3fe00774891b32
yonur <yonurpirdal@hotmail.com>
2018-12-15 11:38:24 +0300

    Msp initialization for timer2

--- TIMER_INPUT_CAPTURE/Src/msp.c
+++ TIMER_INPUT_CAPTURE/Src/msp.c
@@ -23,3 +23,23 @@ void HAL_MspInit(void)
 		HAL_NVIC_SetPriority(BusFault_IRQn, 0, 0);
 		HAL_NVIC_SetPriority(UsageFault_IRQn, 0, 0);
 }
+
+void HAL_TIM_IC_MspInit(TIM_HandleTypeDef *htim) {
+
+	GPIO_InitTypeDef timer2_Channel1_GPIO;
+
+	//Enable peripheral clock of timer2
+	__HAL_RCC_TIM2_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+
+	//Configure GPIO to work as timer2 channel 1
+	timer2_Channel1_GPIO.Pin = GPIO_PIN_0;
+	timer2_Channel1_GPIO.Mode = GPIO_MODE_AF_PP;
+	timer2_Channel1_GPIO.Alternate = GPIO_AF1_TIM2;
+	HAL_GPIO_Init(GPIOA, &timer2_Channel1_GPIO);
+
+	//Set and activate NVIC
+	HAL_NVIC_SetPriority(TIM2_IRQn, 15, 0);
+	HAL_NVIC_EnableIRQ(TIM2_IRQn);
+
+}
