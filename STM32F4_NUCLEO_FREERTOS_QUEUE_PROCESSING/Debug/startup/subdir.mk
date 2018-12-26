d726dda777f325a0ef1b9b4ee732db28791fbaf9
PIBranch <onurpirdal@gtu.edu.tr>
2018-12-26 21:34:55 +0300

    Created Tasks and Queues were successfully compiled.

--- STM32F4_NUCLEO_FREERTOS_QUEUE_PROCESSING/Debug/startup/subdir.mk
+++ STM32F4_NUCLEO_FREERTOS_QUEUE_PROCESSING/Debug/startup/subdir.mk
@@ -15,7 +15,7 @@ startup/%.o: ../startup/%.s
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU GCC Assembler'
 	@echo $(PWD)
-	arm-none-eabi-as -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -I"C:/Workspace/FreeRTOS_Workspace/STM32F4_NUCLEO_FREERTOS_QUEUE_PROCESSING/StdPeriph_Driver/inc" -I"C:/Workspace/FreeRTOS_Workspace/STM32F4_NUCLEO_FREERTOS_QUEUE_PROCESSING/inc" -I"C:/Workspace/FreeRTOS_Workspace/STM32F4_NUCLEO_FREERTOS_QUEUE_PROCESSING/CMSIS/device" -I"C:/Workspace/FreeRTOS_Workspace/STM32F4_NUCLEO_FREERTOS_QUEUE_PROCESSING/CMSIS/core" -g -o "$@" "$<"
+	arm-none-eabi-as -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -I"C:/Users/yonur/GitHub/OpenSTM32WorkBench_Projects/STM32F4_NUCLEO_FREERTOS_QUEUE_PROCESSING/StdPeriph_Driver/inc" -I"C:/Users/yonur/GitHub/OpenSTM32WorkBench_Projects/STM32F4_NUCLEO_FREERTOS_QUEUE_PROCESSING/inc" -I"C:/Users/yonur/GitHub/OpenSTM32WorkBench_Projects/STM32F4_NUCLEO_FREERTOS_QUEUE_PROCESSING/CMSIS/device" -I"C:/Users/yonur/GitHub/OpenSTM32WorkBench_Projects/STM32F4_NUCLEO_FREERTOS_QUEUE_PROCESSING/CMSIS/core" -g -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
